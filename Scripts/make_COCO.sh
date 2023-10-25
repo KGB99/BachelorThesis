6908a3538b45f3bec5bd8f7bc3a4a726c1d3e05f
KGB99 <k.birgi@gmail.com>
2023-10-25 12:36:51 +0200

    update script

--- Scripts/make_COCO.sh
+++ Scripts/make_COCO.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 Path="/Users/kerim/dev/BachelorThesis"
-python3 code/MakeCoco.py --image_parent="/Volumes/cvg-ssd-05/mvpsp/train"
+#python3 code/MakeCoco.py --image_parent="/Volumes/cvg-ssd-05/mvpsp/train"
+python3 code/MakeCoco.py --image_parent="$Path/Data/mvpsp/train" 
