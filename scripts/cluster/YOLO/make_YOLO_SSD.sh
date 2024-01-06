68eac9e46f26da4d1a16e2f2c4e27945d9aa7619
KGB99 <kbirgi@gmail.com>
2024-01-06 16:26:47 +0100

    fix script

---
+++ scripts/cluster/YOLO/make_YOLO_SSD.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#SBATCH -n 1
+#SBATCH --mem-per-cpu=20G
+#SBATCH --time=4:00:00
+
+module load gcc/8.2.0 python/3.8.5
+
+python3 code/makeYOLO.py --train_dir=/cluster/project/infk/cvg/heinj/datasets/bop/mvpsp --labels_dir=/cluster/home/kbirgi/Annotations/yoloSSD/mvpsp --coco_file=/cluster/project/infk/cvg/heinj/students/kbirgi/BachelorThesis/Annotations/trainSSD/modal_labels.json --img_type=png --info_path=/cluster/home/kbirgi/Annotations/yoloSSD
