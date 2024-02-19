b0b2ee549b30859996bfcd992f5d3408ca6e55c6
KGB99 <k.birgi@gmail.com>
2024-02-19 12:45:43 +0100

    update scripts for refinement

---
+++ scripts/cluster/label_scripts/.ipynb_checkpoints/create_ref_2500-checkpoint.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#SBATCH -n 1
+#SBATCH --mem-per-cpu=80G
+#SBATCH --time=4:00:00
+module load gcc/8.2.0 python/3.8.5
+python3 code/makeAnnotations.py --output_dir=refinement_real/2500_labels/train_labels --coco_file=/cluster/project/infk/cvg/heinj/students/kbirgi/Annotations/refinement_real/2500_labels/016006_2500_amodal_labels.json --train_ratio=1.0 --with_val=False
