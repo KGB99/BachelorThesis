e805a979fb387dab5fa46fca11c1ea42945818f6
KGB99 <k.birgi@gmail.com>
2024-02-24 16:39:52 +0100

    fix script for trials

--- scripts/cluster/eval_scripts/eval_pbr_random.sh
+++ scripts/cluster/eval_scripts/eval_pbr_random.sh
@@ -4,4 +4,4 @@
 #SBATCH --time=4:00:00
 module load gcc/8.2.0 python/3.10.4
 
-python3 code/evaluate.py --images_dir=/cluster/project/infk/cvg/heinj/datasets/bop/mvpsp --preds_path=/cluster/project/infk/cvg/heinj/students/kbirgi/BachelorThesis/Annotations/quantitative_evaluation/preds --bbox_preds=pbr_random_and_kinect_bbox_detections.json --mask_preds=pbr_random_and_kinect_mask_detections.json --labels_path=/cluster/project/infk/cvg/heinj/students/kbirgi/BachelorThesis/Annotations/quantitative_evaluation/test_labels/test_annotations_150.json --output=quantitative_evaluation --save_images=True
+python3 code/evaluate.py --images_dir=/cluster/project/infk/cvg/heinj/datasets/bop/mvpsp --preds_path=/cluster/project/infk/cvg/heinj/students/kbirgi/BachelorThesis/Annotations/quant_eval_trials_nogt/preds --bbox_preds=pbr_random_and_kinect_bbox_detections.json --mask_preds=pbr_random_and_kinect_mask_detections.json --labels_path=/cluster/project/infk/cvg/heinj/students/kbirgi/BachelorThesis/Annotations/quant_eval_trials_nogt/amodal_labels_150.json --output=quant_eval_trials_nogt --save_images=True
