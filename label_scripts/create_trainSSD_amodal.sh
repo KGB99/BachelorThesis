86ce1a34d2c0b667fa3f3354b42dd08e56fd8513
KGB99 <k.birgi@gmail.com>
2024-02-28 20:40:24 +0100

    update script

--- label_scripts/create_trainSSD_amodal.sh
+++ label_scripts/create_trainSSD_amodal.sh
@@ -3,4 +3,4 @@
 #SBATCH --mem-per-cpu=80G
 #SBATCH --time=4:00:00
 module load gcc/8.2.0 python/3.8.5
-python3 code/makeAnnotations.py --output_dir=trainSSD/train_labels/amodal --coco_file=/cluster/project/infk/cvg/heinj/students/kbirgi/BachelorThesis/Annotations/trainSSD/amodal_labels.json
+python3 code/makeAnnotations.py --output_dir=trainSSD/train_labels/amodal --coco_file=/cluster/project/infk/cvg/heinj/students/kbirgi/BachelorThesis/Annotations/trainSSD/amodal_labels.json --old_annotations=True
