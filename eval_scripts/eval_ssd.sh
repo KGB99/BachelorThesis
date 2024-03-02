241734f16c81b0372450bc89476fb51a9fe5d3f6
KGB99 <k.birgi@gmail.com>
2024-03-02 16:21:31 +0100

    update scripts

--- eval_scripts/eval_ssd.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#SBATCH -n 1
-#SBATCH --mem-per-cpu=20G
-#SBATCH --time=4:00:00
-module load gcc/8.2.0 python/3.10.4
-
-python3 code/preprocess_eval.py --yolact=True --images_dir=/cluster/project/infk/cvg/heinj/datasets/bop/mvpsp --preds_path=/cluster/project/infk/cvg/heinj/students/kbirgi/BachelorThesis/Annotations/quant_eval_trials_nogt/preds --bbox_preds=pbr_random_and_kinect_bbox_detections.json --mask_preds=pbr_random_and_kinect_mask_detections.json --labels_path=/cluster/project/infk/cvg/heinj/students/kbirgi/BachelorThesis/Annotations/quant_eval_trials_nogt/amodal_labels_150.json --output=quant_eval_trials_nogt --save_images=True
