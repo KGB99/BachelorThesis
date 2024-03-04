c6bf3d1cd96fb139aa59193ee877917fa175fd42
KGB99 <k.birgi@gmail.com>
2024-03-04 11:25:56 +0100

    update scripts

---
+++ yolo_scripts/make_yolo_pbr.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#SBATCH -n 1
+#SBATCH --mem-per-cpu=100G
+#SBATCH --time=4:00:00
+
+module load gcc/8.2.0 python/3.8.5
+python3 code/makeYOLO.py --train_dir=/cluster/project/infk/cvg/heinj/datasets/bop/mvpsp --labels_dir=/cluster/home/kbirgi/Annotations/yolopbr/mvpsp --coco_file=/cluster/project/infk/cvg/heinj/students/kbirgi/BachelorThesis/Annotations/train_pbr_random_and_kinect/amodal_labels.json --img_type=jpg --info_path=/cluster/home/kbirgi/Annotations/yolopbr
