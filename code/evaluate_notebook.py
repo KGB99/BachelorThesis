6781b92e6a3be224f477fa298f574c6591bf27b3
KGB99 <k.birgi@gmail.com>
2024-03-04 11:26:35 +0100

    update eval to include map and ap

--- code/evaluate_notebook.py
+++ code/evaluate_notebook.py
@@ -8,11 +8,12 @@ import pycocotools.mask as maskUtils
 from matplotlib import pyplot as plt
 import argparse
 from pathlib import Path
+from sklearn import metrics
 
-def eval_yolact(path_to_eval_values):
+def eval_yolact(path_to_eval_values, iou_threshold, PLOT):
     df = pd.read_csv(path_to_eval_values)
     p = Path(eval_file_path)
-    output = ''
+    output = f"IoU-Threshold: {iou_threshold}\n"
     #print(df.columns.tolist()) : 
     #['pred_powerdrill', 'pred_screwdriver', 'image_file', 'gt_screwdriver', 'pred_screwdriver_conf', 'bbox_iou_screwdriver', 'gt_powerdrill', 'pred_powerdrill_conf', 'bbox_iou_powerdrill']
 
@@ -21,7 +22,6 @@ def eval_yolact(path_to_eval_values):
 
     # first do everything for bboxes
     eval_dict = {}
-    iou_threshold = 0.5
     for conf_threshold in threshold_list:
         
         #calculate confusion matrix for powerdrill bbox
@@ -69,13 +69,13 @@ def eval_yolact(path_to_eval_values):
             eval_dict[conf_threshold]['screwdriver']['recall'] = len(screwdriver_TP_df) / (len(screwdriver_TP_df) + len(screwdriver_FN_df))
         
         if (power_undefined or screw_undefined):
-            output += (f"Threshold: {conf_threshold:.2f} | Recall: Undefined | Precision: Undefined "
+            output += (f"Confidence-Threshold: {conf_threshold:.2f} | Recall: Undefined | Precision: Undefined "
             f"| TP: {len(powerdrill_TP_df) + len(screwdriver_TP_df):4} | FN: {len(powerdrill_FN_df) + len(screwdriver_FN_df):4} | FP: {len(powerdrill_FP_df) + len(screwdriver_FP_df):4} \n")
             continue
         eval_dict[conf_threshold]['total'] = {}
         eval_dict[conf_threshold]['total']['precision'] = (eval_dict[conf_threshold]['screwdriver']['precision'] + eval_dict[conf_threshold]['powerdrill']['precision']) / 2
         eval_dict[conf_threshold]['total']['recall'] = (eval_dict[conf_threshold]['screwdriver']['recall'] + eval_dict[conf_threshold]['powerdrill']['recall']) / 2
-        output += (f"Threshold: {conf_threshold:.2f} | Recall: {round(eval_dict[conf_threshold]['total']['recall'], 2):.2f} | Precision: {round(eval_dict[conf_threshold]['total']['precision'], 2):.2f} "
+        output += (f"Confidence-Threshold: {conf_threshold:.2f} | Recall: {round(eval_dict[conf_threshold]['total']['recall'], 2):.2f} | Precision: {round(eval_dict[conf_threshold]['total']['precision'], 2):.2f} "
             f"| TP: {len(powerdrill_TP_df) + len(screwdriver_TP_df):4} | FN: {len(powerdrill_FN_df) + len(screwdriver_FN_df):4} | FP: {len(powerdrill_FP_df) + len(screwdriver_FP_df):4} \n")
         
 
@@ -97,37 +97,56 @@ def eval_yolact(path_to_eval_values):
             screw_recall_list.append(eval_dict[conf_threshold]['screwdriver']['recall'])
             screw_precision_list.append(eval_dict[conf_threshold]['screwdriver']['precision'])
 
+    # the documentation for sklearn.metrics says the lists have to be sorted monotonically at the recall list
+    # source: https://scikit-learn.org/stable/modules/generated/sklearn.metrics.auc.html#sklearn.metrics.auc
+    # therefore we sort both lists before calculating area under curve (auc)
+    np_pow_recall_list = np.array(pow_recall_list)
+    np_pow_prec_list = np.array(pow_precision_list)
+    sorted_powerdrill = np.argsort(np_pow_recall_list)
+    sorted_pow_recall = np_pow_recall_list[sorted_powerdrill]
+    sorted_pow_prec = np_pow_prec_list[sorted_powerdrill]
+    AP_powerdrill = metrics.auc(sorted_pow_recall, sorted_pow_prec)
 
+    np_screw_recall_list = np.array(screw_recall_list)
+    np_screw_prec_list = np.array(screw_precision_list)
+    sorted_screwdriver = np.argsort(np_screw_recall_list)
+    sorted_screw_recall = np_screw_recall_list[sorted_screwdriver]
+    sorted_screw_precision = np_screw_prec_list[sorted_screwdriver]
+    AP_screwdriver = metrics.auc(sorted_screw_recall, sorted_screw_precision)
 
-    fig, ax = plt.subplots()
-    ax.plot(total_recall_list, total_precision_list,marker='o', linestyle='-')
-    ax.set_title("Total Precision-Recall Curve : YOLO-" + str(p.parent.name))
-    ax.set_xlabel("Recall")
-    ax.set_ylabel("Precision")
-    ax.grid(True)
-    fig.savefig(str(p.parent) + '/' + str(p.parent.name) + '_total-prec-rec.png', dpi=300)
+    mAP = 0.5 * (AP_powerdrill + AP_screwdriver)
+    output += (f"mAP: {mAP} \n\n\n-------------------------------\n\n\n")
 
-    fig, ax = plt.subplots()
-    ax.plot(pow_recall_list, pow_precision_list, marker='o', linestyle='-')
-    ax.set_title("Powerdrill Precision-Recall Curve : YOLO-" + str(p.parent.name))
-    ax.set_xlabel("Recall")
-    ax.set_ylabel("Precision")
-    ax.grid(True)
-    fig.savefig(str(p.parent) + '/' + str(p.parent.name) + '_power-prec-rec.png', dpi=300)
+    if PLOT:
+        fig, ax = plt.subplots()
+        ax.plot(total_recall_list, total_precision_list,marker='o', linestyle='-')
+        ax.set_title("Total Precision-Recall Curve : YOLO-" + str(p.parent.name))
+        ax.set_xlabel("Recall")
+        ax.set_ylabel("Precision")
+        ax.grid(True)
+        fig.savefig(str(p.parent) + '/' + str(p.parent.name) + '_total-prec-rec.png', dpi=300)
 
-    fig,ax = plt.subplots()
-    ax.plot(screw_recall_list, screw_precision_list, marker='o', linestyle='-')
-    ax.set_title("Screwdriver Precision-Recall Curve : YOLO-" + str(p.parent.name))
-    ax.set_xlabel("Recall")
-    ax.set_ylabel("Precision")
-    ax.grid(True)
-    fig.savefig(str(p.parent) + '/' + str(p.parent.name) + '_screw-prec-rec.png', dpi=300)
+        fig, ax = plt.subplots()
+        ax.plot(pow_recall_list, pow_precision_list, marker='o', linestyle='-')
+        ax.set_title("Powerdrill Precision-Recall Curve : YOLO-" + str(p.parent.name))
+        ax.set_xlabel("Recall")
+        ax.set_ylabel("Precision")
+        ax.grid(True)
+        fig.savefig(str(p.parent) + '/' + str(p.parent.name) + '_power-prec-rec.png', dpi=300)
+
+        fig,ax = plt.subplots()
+        ax.plot(screw_recall_list, screw_precision_list, marker='o', linestyle='-')
+        ax.set_title("Screwdriver Precision-Recall Curve : YOLO-" + str(p.parent.name))
+        ax.set_xlabel("Recall")
+        ax.set_ylabel("Precision")
+        ax.grid(True)
+        fig.savefig(str(p.parent) + '/' + str(p.parent.name) + '_screw-prec-rec.png', dpi=300)
     return output
 
-def eval_yolo(path_to_eval_values):
+def eval_yolo(path_to_eval_values, iou_threshold, PLOT):
     df = pd.read_csv(path_to_eval_values)
     p = Path(eval_file_path)
-    output = ''
+    output = f"IoU-Threshold: {iou_threshold}\n"
     #print(df.columns.tolist()) : 
     #['pred_powerdrill', 'pred_screwdriver', 'image_file', 'gt_screwdriver', 'pred_screwdriver_conf', 'bbox_iou_screwdriver', 'gt_powerdrill', 'pred_powerdrill_conf', 'bbox_iou_powerdrill']
 
@@ -136,7 +155,6 @@ def eval_yolo(path_to_eval_values):
 
     # first do everything for bboxes
     eval_dict = {}
-    iou_threshold = 0.5
     for conf_threshold in threshold_list:
         
         #calculate confusion matrix for powerdrill bbox
@@ -160,21 +178,40 @@ def eval_yolo(path_to_eval_values):
         eval_dict[conf_threshold]['powerdrill']['TP'] = len(powerdrill_TP_df)
         eval_dict[conf_threshold]['powerdrill']['FP'] = len(powerdrill_FP_df) 
         eval_dict[conf_threshold]['powerdrill']['FN'] = len(powerdrill_FN_df)
-        eval_dict[conf_threshold]['powerdrill']['precision'] = len(powerdrill_TP_df) / (len(powerdrill_TP_df) + len(powerdrill_FP_df))
-        eval_dict[conf_threshold]['powerdrill']['recall'] = len(powerdrill_TP_df) / (len(powerdrill_TP_df) + len(powerdrill_FN_df))
+        PWD_TP = len(powerdrill_TP_df)
+        PWD_FP = len(powerdrill_FP_df)
+        PWD_FN = len(powerdrill_FN_df)
+        power_undefined = True
+        if ((PWD_TP + PWD_FP != 0) & (PWD_TP + PWD_FN != 0)):
+            power_undefined = False
+            eval_dict[conf_threshold]['powerdrill']['precision'] = len(powerdrill_TP_df) / (len(powerdrill_TP_df) + len(powerdrill_FP_df))
+        #if (TP + FN != 0):
+            eval_dict[conf_threshold]['powerdrill']['recall'] = len(powerdrill_TP_df) / (len(powerdrill_TP_df) + len(powerdrill_FN_df))
 
         eval_dict[conf_threshold]['screwdriver'] = {}
         eval_dict[conf_threshold]['screwdriver']['TP'] = len(screwdriver_TP_df)
         eval_dict[conf_threshold]['screwdriver']['FP'] = len(screwdriver_FP_df)
         eval_dict[conf_threshold]['screwdriver']['FN'] = len(screwdriver_FN_df)
-        eval_dict[conf_threshold]['screwdriver']['precision'] = len(screwdriver_TP_df) / (len(screwdriver_TP_df) + len(screwdriver_FP_df))
-        eval_dict[conf_threshold]['screwdriver']['recall'] = len(screwdriver_TP_df) / (len(screwdriver_TP_df) + len(screwdriver_FN_df))
+        SCR_TP = len(screwdriver_TP_df)
+        SCR_FP = len(screwdriver_FP_df)
+        SCR_FN = len(screwdriver_FN_df)
+        screw_undefined = True
+        if ((SCR_TP + SCR_FP != 0) & (SCR_TP + SCR_FN != 0)):
+            screw_undefined = False
+            eval_dict[conf_threshold]['screwdriver']['precision'] = len(screwdriver_TP_df) / (len(screwdriver_TP_df) + len(screwdriver_FP_df))
+        #if (TP + FN != 0):
+            eval_dict[conf_threshold]['screwdriver']['recall'] = len(screwdriver_TP_df) / (len(screwdriver_TP_df) + len(screwdriver_FN_df))
         
+        if (power_undefined or screw_undefined):
+            output += (f"Confidence-Threshold: {conf_threshold:.2f} | Recall: Undefined | Precision: Undefined "
+            f"| TP: {len(powerdrill_TP_df) + len(screwdriver_TP_df):4} | FN: {len(powerdrill_FN_df) + len(screwdriver_FN_df):4} | FP: {len(powerdrill_FP_df) + len(screwdriver_FP_df):4} \n")
+            continue    
+
         eval_dict[conf_threshold]['total'] = {}
         eval_dict[conf_threshold]['total']['precision'] = (eval_dict[conf_threshold]['screwdriver']['precision'] + eval_dict[conf_threshold]['powerdrill']['precision']) / 2
         eval_dict[conf_threshold]['total']['recall'] = (eval_dict[conf_threshold]['screwdriver']['recall'] + eval_dict[conf_threshold]['powerdrill']['recall']) / 2
 
-        output += (f"Threshold: {conf_threshold:.2f} | Recall: {round(eval_dict[conf_threshold]['total']['recall'], 2):.2f} | Precision: {round(eval_dict[conf_threshold]['total']['precision'], 2):.2f} "
+        output += (f"Confidence-Threshold: {conf_threshold:.2f} | Recall: {round(eval_dict[conf_threshold]['total']['recall'], 2):.2f} | Precision: {round(eval_dict[conf_threshold]['total']['precision'], 2):.2f} "
             f"| TP: {len(powerdrill_TP_df) + len(screwdriver_TP_df):4} | FN: {len(powerdrill_FN_df) + len(screwdriver_FN_df):4} | FP: {len(powerdrill_FP_df) + len(screwdriver_FP_df):4} \n")
         
 
@@ -186,38 +223,58 @@ def eval_yolo(path_to_eval_values):
     screw_recall_list = []
     screw_precision_list = []
     for conf_threshold in eval_dict:
-        total_recall_list.append(eval_dict[conf_threshold]['total']['recall'])
-        total_precision_list.append(eval_dict[conf_threshold]['total']['precision'])
-        pow_recall_list.append(eval_dict[conf_threshold]['powerdrill']['recall'])
-        pow_precision_list.append(eval_dict[conf_threshold]['powerdrill']['precision'])
-        screw_recall_list.append(eval_dict[conf_threshold]['screwdriver']['recall'])
-        screw_precision_list.append(eval_dict[conf_threshold]['screwdriver']['precision'])
+        if 'total' in eval_dict[conf_threshold]:
+            total_recall_list.append(eval_dict[conf_threshold]['total']['recall'])
+            total_precision_list.append(eval_dict[conf_threshold]['total']['precision'])
+        if (('recall' in eval_dict[conf_threshold]['powerdrill']) and ('precision' in eval_dict[conf_threshold]['powerdrill'])):
+            pow_recall_list.append(eval_dict[conf_threshold]['powerdrill']['recall'])
+            pow_precision_list.append(eval_dict[conf_threshold]['powerdrill']['precision'])
+        if (('recall' in eval_dict[conf_threshold]['screwdriver']) and ('precision' in eval_dict[conf_threshold]['screwdriver'])):
+            screw_recall_list.append(eval_dict[conf_threshold]['screwdriver']['recall'])
+            screw_precision_list.append(eval_dict[conf_threshold]['screwdriver']['precision'])
 
+    # same method as above
+    np_pow_recall_list = np.array(pow_recall_list)
+    np_pow_prec_list = np.array(pow_precision_list)
+    sorted_powerdrill = np.argsort(np_pow_recall_list)
+    sorted_pow_recall = np_pow_recall_list[sorted_powerdrill]
+    sorted_pow_prec = np_pow_prec_list[sorted_powerdrill]
+    AP_powerdrill = metrics.auc(sorted_pow_recall, sorted_pow_prec)
 
+    np_screw_recall_list = np.array(screw_recall_list)
+    np_screw_prec_list = np.array(screw_precision_list)
+    sorted_screwdriver = np.argsort(np_screw_recall_list)
+    sorted_screw_recall = np_screw_recall_list[sorted_screwdriver]
+    sorted_screw_precision = np_screw_prec_list[sorted_screwdriver]
+    AP_screwdriver = metrics.auc(sorted_screw_recall, sorted_screw_precision)
 
-    fig, ax = plt.subplots()
-    ax.plot(total_recall_list, total_precision_list,marker='o', linestyle='-')
-    ax.set_title("Total Precision-Recall Curve : YOLO-" + str(p.parent.name))
-    ax.set_xlabel("Recall")
-    ax.set_ylabel("Precision")
-    ax.grid(True)
-    fig.savefig(str(p.parent) + '/' + str(p.parent.name) + '_total-prec-rec.png', dpi=300)
+    mAP = 0.5 * (AP_powerdrill + AP_screwdriver)
+    output += (f"mAP: {mAP} \n\n\n-------------------------------\n\n\n")
 
-    fig, ax = plt.subplots()
-    ax.plot(pow_recall_list, pow_precision_list, marker='o', linestyle='-')
-    ax.set_title("Powerdrill Precision-Recall Curve : YOLO-" + str(p.parent.name))
-    ax.set_xlabel("Recall")
-    ax.set_ylabel("Precision")
-    ax.grid(True)
-    fig.savefig(str(p.parent) + '/' + str(p.parent.name) + '_power-prec-rec.png', dpi=300)
+    if PLOT:
+        fig, ax = plt.subplots()
+        ax.plot(total_recall_list, total_precision_list,marker='o', linestyle='-')
+        ax.set_title("Total Precision-Recall Curve : YOLO-" + str(p.parent.name))
+        ax.set_xlabel("Recall")
+        ax.set_ylabel("Precision")
+        ax.grid(True)
+        fig.savefig(str(p.parent) + '/' + str(p.parent.name) + '_total-prec-rec.png', dpi=300)
 
-    fig,ax = plt.subplots()
-    ax.plot(screw_recall_list, screw_precision_list, marker='o', linestyle='-')
-    ax.set_title("Screwdriver Precision-Recall Curve : YOLO-" + str(p.parent.name))
-    ax.set_xlabel("Recall")
-    ax.set_ylabel("Precision")
-    ax.grid(True)
-    fig.savefig(str(p.parent) + '/' + str(p.parent.name) + '_screw-prec-rec.png', dpi=300)
+        fig, ax = plt.subplots()
+        ax.plot(pow_recall_list, pow_precision_list, marker='o', linestyle='-')
+        ax.set_title("Powerdrill Precision-Recall Curve : YOLO-" + str(p.parent.name))
+        ax.set_xlabel("Recall")
+        ax.set_ylabel("Precision")
+        ax.grid(True)
+        fig.savefig(str(p.parent) + '/' + str(p.parent.name) + '_power-prec-rec.png', dpi=300)
+
+        fig,ax = plt.subplots()
+        ax.plot(screw_recall_list, screw_precision_list, marker='o', linestyle='-')
+        ax.set_title("Screwdriver Precision-Recall Curve : YOLO-" + str(p.parent.name))
+        ax.set_xlabel("Recall")
+        ax.set_ylabel("Precision")
+        ax.grid(True)
+        fig.savefig(str(p.parent) + '/' + str(p.parent.name) + '_screw-prec-rec.png', dpi=300)
     
     return output
 
@@ -236,7 +293,11 @@ if __name__ == '__main__':
         yolo_folders = os.listdir(parent_path)
         for folder in yolo_folders:
             eval_file_path = parent_path + '/' + folder + '/eval_values.csv'
-            output = eval_yolo(eval_file_path)
+            threshold_list = list(range(50,100,5))
+            threshold_list = [x/100 for x in threshold_list]
+            output = ''
+            for iou_threshold in threshold_list:
+                output += eval_yolo(eval_file_path, iou_threshold, False)
             with open(parent_path + '/' + folder + '/eval_output.txt', 'w') as f:
                 f.write(output)
             print(output)
@@ -248,7 +309,11 @@ if __name__ == '__main__':
         yolact_folders = os.listdir(parent_path)
         for folder in yolact_folders:
             eval_file_path = parent_path + '/' + folder + '/eval_values.csv'
-            output = eval_yolact(eval_file_path)
+            threshold_list = list(range(50,100,5))
+            threshold_list = [x/100 for x in threshold_list]
+            output = ''
+            for iou_threshold in threshold_list:
+                output += eval_yolact(eval_file_path, iou_threshold, PLOT=False)
             with open(parent_path + '/' + folder + '/eval_output.txt', 'w') as f:
                 f.write(output)
             print(output)
