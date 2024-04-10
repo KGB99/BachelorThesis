2be12be82f066b9308702024fa30b8e433cc0e98
KGB99 <k.birgi@gmail.com>
2024-04-10 11:11:55 +0200

    label scripts pre cleanup

---
+++ label_scripts/create_unsupervised_ref_aug.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#SBATCH -n 1
+#SBATCH --mem-per-cpu=80G
+#SBATCH --time=4:00:00
+module load gcc/8.2.0 python/3.8.5
+python3 code/makeAnnotations.py --output_dir=supervised_pbr_ref_aug --coco_file=/cluster/project/infk/cvg/heinj/students/kbirgi/Annotations/gen_annotations/stride_50_pbr_ref_all_24000/generated_labels.json --old_annotations=True
